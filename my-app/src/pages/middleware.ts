5f4823bfe461072e257f284161bb2a49b0d51594
Annisa Kurniawati <annisak0708@gmail.com>
2026-04-07 07:09:26 +0700

    update JS13

---
+++ my-app/src/pages/middleware.ts
@@ -0,0 +1,16 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const isLogin = false;
+    if (isLogin) {
+        return NextResponse.next();
+    } else {
+        return NextResponse.redirect(new URL("/auth/login", request.url));
+    }
+    // return NextResponse.redirect(new URL("/", request.url));
+    // return NextResponse.next();
+}
+    export const config = {
+    matcher: ["/produk/:path*", "/about/:path*"],
+};
--- my-app/src/pages/produk/index.tsx
+++ my-app/src/pages/produk/index.tsx
@@ -1,20 +1,22 @@
+import fetcher from "@/utils/swr/fetcher";
 import TampilanProduk from "@/views/produk";
+import { useRouter } from "next/router";
+import { useState } from "react";
 import useSWR from "swr";
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Kategori = () => {
+  const {push} = useRouter();
+  const [products, setProducts] = useState([]);
   const { data, error, isLoading } = useSWR("/api/produk", fetcher);
 
-  const products = data?.data || [];
-
-  if (error) {
-    console.error("Terjadi kesalahan saat mengambil data:", error);
-  }
+  // const products = data?.data || [];
 
   return (
     <div>
-      <TampilanProduk products={products} />
+      <TampilanProduk products={isLoading ? [] : data?.data || []} />
     </div>
   );
 };
