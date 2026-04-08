51e609cc7a6b7e361539e6217d857b0731d9717b
Annisa Kurniawati <annisak0708@gmail.com>
2026-04-08 07:14:45 +0700

    JS14

--- my-app/src/middleware.ts
+++ my-app/src/middleware.ts
@@ -1,23 +1,14 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import withAuth from "./Middleware/withAuth";
 
-export function middleware(request: NextRequest) {
-  const isLogin = request.cookies.get("isLogin")?.value;
-
-  const pathname = request.nextUrl.pathname;
-
-  // daftar route yang ingin diproteksi
-  const protectedRoutes = ["/produk", "/about"];
-
-  const isProtected = protectedRoutes.some((route) =>
-    pathname.startsWith(route)
-  );
-
-  // belum login + akses route protected
-  if (isProtected && isLogin !== "true") {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
-  }
-
-  // selain itu, lanjut
+export function mainMiddleware(request: NextRequest) {
   return NextResponse.next();
 }
+
+// Bungkus mainMiddleware dengan withAuth
+export default withAuth(mainMiddleware, ["/profile", "/produk", "/about"]);
+
+export const config = {
+  matcher: ["/profile", "/produk", "/about"],
+};
--- my-app/src/pages/profile/index.tsx
+++ my-app/src/pages/profile/index.tsx
@@ -1,10 +1,11 @@
-const ProfilePage = () => {
-  return (
-    <div>
-      <h1>Halaman Profile</h1>
-      <p>Ini adalah halaman profile.</p>
-    </div>
-  );
-};
-
-export default ProfilePage;
+import { useSession } from "next-auth/react";
+const HalamanProfile = () => {
+const { data }: any = useSession();
+return (
+<div>
+<h1>Halaman Profile</h1><br />
+<h1>Selamat Datang {data?.user?.fullname}</h1>
+</div>
+)
+}
+export default HalamanProfile;
